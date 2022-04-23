fe337f5ea651776b9163c27b1d423e14e6bcccd7
Wahid <wejaz@ssidecisions.com>
2022-04-23 16:00:18 +0500

    Updated unit tests for App component.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom/client'
+import { render, screen , cleanup} from '@testing-library/react';
 import App from './App';
+import TestRenderer from 'react-test-renderer';
+import {create,act} from 'react-test-renderer';
+import "@testing-library/jest-dom/extend-expect"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+afterEach(cleanup)
+test('Application is rendered', () => {
+    const div = document.createElement('div');
+    const root = ReactDOM.createRoot(div)
+})
+
+test('Application login tab renders correctly', ()=> {
+    const tree = TestRenderer.create(<App loginstatus={false}/>).toJSON()
+    expect(tree).toMatchSnapshot()
+})
+
