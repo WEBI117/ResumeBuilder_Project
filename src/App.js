6aec06474bcc97eab2df2181d62725ae742f44b8
Wahid <wejaz@ssidecisions.com>
2022-04-23 16:02:25 +0500

    Updated rendering logic to account for newly added components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
 import Logincomponent from './components/Logincomponent/login'
+import Resumebuilder from './components/Resumebuildercomponent/resumebuilder'
 import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
-    let [islogged,updateLoginStatus]=useState(false);
-    let content;
+function App(props) {
+    // react hook to track state for application login status.
+    let [isloggedin,updateLoginStatus]=useState(props.loginstatus);
+    // function to be passed to children page components to trigger loggin status changes in parent component.
     let loginStatusUpdateEvent = (e,status) => {
         updateLoginStatus(status)
     }
+    // component placeholder determined by login status. Will display login page incase user has not logged in already.
+    let content;
 
-    if(!islogged){
+    if(!isloggedin){
         content=<Logincomponent loginStatusHandler={loginStatusUpdateEvent}/>
     }
     else{
-        content=<p>Logged In</p>
+        content=<Resumebuilder/>
     }
   return (
-
     <div className="App">
       {content}
     </div>
