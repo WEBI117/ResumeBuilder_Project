779fe5765baababf05ad0e6647f1ca30784545bb
Wahid <wejaz@ssidecisions.com>
2022-04-25 13:42:09 +0500

    Added loginstatus change functionality for logout event.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Logincomponent from './components/Logincomponent/login'
 import Resumebuilder from './components/Resumebuildercomponent/resumebuilder'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App(props) {
     // react hook to track state for application login status.
     let [isloggedin,updateLoginStatus]=useState(props.loginstatus);
+    // component placeholder determined by login status. Will display login page incase user has not logged in already.
+    let [content,setContent] = useState();
+
+    useEffect(() => {
+        if(!isloggedin){
+            setContent(<Logincomponent loginStatusHandler={loginStatusUpdateEvent}/>)
+        }
+        else{
+            setContent(<Resumebuilder loginStatusUpdateEvent={loginStatusUpdateEvent}/>)
+        }
+    },[isloggedin])
+
     // function to be passed to children page components to trigger loggin status changes in parent component.
     let loginStatusUpdateEvent = (e,status) => {
         updateLoginStatus(status)
     }
-    // component placeholder determined by login status. Will display login page incase user has not logged in already.
-    let content;
+    // 
 
-    if(!isloggedin){
-        content=<Logincomponent loginStatusHandler={loginStatusUpdateEvent}/>
-    }
-    else{
-        content=<Resumebuilder/>
-    }
+    //if(!isloggedin){
+    //    content=<Logincomponent loginStatusHandler={loginStatusUpdateEvent}/>
+    //}
+    //else{
+    //    content=<Resumebuilder/>
+    //}
   return (
     <div className="App">
       {content}
